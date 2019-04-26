b1608cf656910e324528eca9413c8afb20584f9b
Soumil Khandelwal <31521503+soumilk@users.noreply.github.com>
2019-04-26 19:06:27 +0530

    Create Functions.cpp

---
+++ C++/Introduction/Functions.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/*
+Add `int max_of_four(int a, int b, int c, int d)` here.
+*/
+
+int main() {
+    int a, b, c, d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    int ans = a;
+    if ( ans< b)
+        ans=b;
+    if ( ans< c)
+        ans=c;
+    if ( ans< d)
+        ans=d;
+    
+    printf("%d", ans);
+    
+    return 0;
+}
