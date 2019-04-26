14f2558c0eaafade70992369b3110d507a86d90f
Soumil Khandelwal <31521503+soumilk@users.noreply.github.com>
2019-04-26 19:07:17 +0530

    Create Pointer.cpp

---
+++ C++/Introduction/Pointer.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void update(int *a,int *b) {
+    // Complete this function
+      *a = *a + *b ;
+       *b= *a - *b -*b ;
+        if (*b <0 )
+            *b = -*b;
+}
+
+int main() {
+    int a, b;
+    int *pa = &a, *pb = &b;
+    
+    scanf("%d %d", &a, &b);
+    update(pa, pb);
+    printf("%d\n%d", a, b);
+
+    return 0;
+}
