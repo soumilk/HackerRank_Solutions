c6160aa84e6e53c7c0e02078f4e9887083ff598f
Soumil Khandelwal <31521503+soumilk@users.noreply.github.com>
2019-05-25 19:34:42 +0530

    Create Write a function.py

---
+++ Python/Write a function.py
@@ -0,0 +1,12 @@
+def is_leap(year):
+    leap = False
+    if year%4==0:
+        leap=True
+        if year%100==0 and year%400==0:
+            leap=True
+        if year%100==0 and year%400!=0:
+            leap=False
+    # Write your logic here
+    
+    return leap
+
